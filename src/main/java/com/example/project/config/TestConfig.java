9085469b8d49746974dc2b2323136f240b984e23
Calil Sousa Mattos <contatocsmpm@gmail.com>
2023-09-29 10:30:54 -0300

    Payment, one-to-one association

--- src/main/java/com/example/project/config/TestConfig.java
+++ src/main/java/com/example/project/config/TestConfig.java
@@ -12,6 +12,7 @@ import com.example.project.entities.User;
 import com.example.project.entities.enums.OrderStatus;
 import com.example.project.entities.Order;
 import com.example.project.entities.OrderItem;
+import com.example.project.entities.Payment;
 import com.example.project.entities.Product;
 import com.example.project.entities.Category;
 
@@ -81,5 +82,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+    
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
     }
 }
